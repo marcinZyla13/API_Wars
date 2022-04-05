cc5d01fd62bc0584cd13ba23e92817b678e338f4
marcinZyla13 <82803199+marcinZyla13@users.noreply.github.com>
2022-04-05 18:43:01 +0200

    data format conversion and page validation

--- static/js/Main_Page.js
+++ static/js/Main_Page.js
@@ -1,12 +1,15 @@
+let amountOfObjects = 0;
+let page = 1;
+
 window.onload = function(){
-    collectInformationAboutPlanets();
+    collectInformationAboutPlanets("https://swapi.dev/api/planets");
+    addEventListeners();
 }
 
 
-function collectInformationAboutPlanets()
+function collectInformationAboutPlanets(uri)
 {
-
-    const response = fetch  ("https://swapi.dev/api/planets", {
+    const response = fetch  (uri, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -16,26 +19,70 @@ function collectInformationAboutPlanets()
     }).then(response => response.json())
         .then( data => displayData(data))
 
-
 }
 
 function displayData(data) {
+    amountOfObjects = data.count;
     let row;
     let table = document.getElementById('table');
-    let counter = 0;
     data['results'].forEach(element => {
             row += `<tr>
                     <td>${element['name']}</td>
                     <td>${element['diameter']} km</td>
                     <td>${element['climate']}</td>
-                    <td>${element['terrain']}</td>\
-                    <td>${element['surface_water']}</td>\
-                    <td>${element['population']}</td>\
-                    </tr>`
+                    <td>${element['terrain']}</td>`
+                    row+= (element['surface_water'] === 'unknown' ? `<td>${element['surface_water']} </td>` : `<td>${element['surface_water']} %</td>`)
+                    row+= (element['population'] === 'unknown' ? `<td>${element['population']} </td>` : `<td>${convertPopulationNumber(element['population'])} people</td>`)
+                    row+= `</tr>`
     })
     table.innerHTML = row;
+    convertPopulationNumber();
 
 }
+
+function convertPopulationNumber(population)
+{
+    let internationalNumberFormat = new Intl.NumberFormat('en-US')
+    return internationalNumberFormat.format(population);
+}
+
+function addEventListeners()
+{
+    let uri;
+
+    document.getElementById('next').addEventListener('click',function()
+    {
+        checkPageAvailability("+");
+        uri = `https://swapi.dev/api/planets/?page=${page}`;
+        collectInformationAboutPlanets(uri);
+    })
+    document.getElementById('previous').addEventListener('click',function()
+    {
+        checkPageAvailability("-");
+        uri = `https://swapi.dev/api/planets/?page=${page}`;
+        collectInformationAboutPlanets(uri);
+    })
+
+
+}
+
+function checkPageAvailability(operation)
+{
+    let objectsOnThePage = 10;
+    let maxAmountOfPages = amountOfObjects/objectsOnThePage;
+    if(operation === "-")
+    {
+        if(page - 1 >=1)
+            page-=1;
+    }
+    else
+    {
+        if(page + 1 <= maxAmountOfPages)
+            page+=1;
+    }
+
+}
+
 
 
 
